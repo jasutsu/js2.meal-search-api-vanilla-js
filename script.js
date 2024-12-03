592228c27a48ed3ef15c4433f14ee0f4f30b8f1f
Jashan-Windows <jashanbhullar376@gmail.com>
2024-12-03 11:30:15 +0530

    feat: declare variables

---
+++ script.js
@@ -0,0 +1,5 @@
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const mealList = document.getElementById('meal');
+const searchInput = document.getElementById('search-input');
+const searchForm = document.querySelector('.meal-search-box');
