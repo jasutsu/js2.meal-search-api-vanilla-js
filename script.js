4b2a0e5b27dcab67d0d1fa00e47761129202f139
Jashan-Windows <jashanbhullar376@gmail.com>
2024-12-03 10:12:35 +0530

    feat: fill dom with meal list

--- script.js
+++ script.js
@@ -4,23 +4,40 @@ const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
-
 const getMealList = () => {
     const searchText = searchInput.value.trim();
     const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchText}`;
+    console.log('Start Fetch');
     fetch(url)
         .then(response => response.json())
-        .then(data => data.meals)
-        .catch(error => console.error('Fetch Error: ', error));
+        .then(data => {
+            mealList.innerHTML = "";
+            const meals = data.meals;
 
-        // idMeal
-        // : 
-        // "52940"
-        // strMeal
-        // : 
-        // "Brown Stew Chicken"
-        // strMealThumb
-        
+            if (meals) {
+                meals.forEach(meal => {
+                    const mealItem = document.createElement('div');
+                    mealItem.classList.add('meal-item');
+                    mealItem.dataset.id = meal.idMeal;
+                    mealItem.innerHTML = `
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    `;
+                    mealList.appendChild(mealItem);
+                });
+                mealList.classList.remove('notFound');
+            } else {
+                mealList.innerHTML = "Sorry, we didn't find any meal";
+                mealList.classList.add('notFound');
+            }
+            console.log('Finish Fetch');
+        })
+        .catch(error => console.error('Fetch Error: ', error));
 };
 
 const getMealRecipe = () => {
