b4ddf8a27ff6edba8c05f36a5d7cc0c6ca0b57f4
Jashan-Windows <jashanbhullar376@gmail.com>
2024-12-03 12:31:07 +0530

    feat: get meal recipe

--- script.js
+++ script.js
@@ -41,8 +41,41 @@ const getMealList = (e) => {
             }
             mealList.innerHTML = markUp;
         })
-        .catch(error => console.error("Fetch Error: ", error));
+        .catch(error => console.error("Meal List Fetch Error: ", error));
 
 };
 
+const getMealRecipe = (e) => {
+    e.preventDefault();
+    const selectedElement = e.target;
+    if(selectedElement.classList.contains('recipe-btn')){
+        const mealId = selectedElement.parentElement.parentElement.dataset.id;
+        fetch(lookupUrl + mealId)
+            .then(response => response.json())
+            .then(data => {
+                const mealRecipe = data.meals[0];
+                mealDetailsContent.innerHTML = `
+                    <h2 class = "recipe-title">${mealRecipe.strMeal}</h2>
+                    <p class = "recipe-category">${mealRecipe.strCategory}</p>
+                    <div class = "recipe-instruct">
+                        <h3>Instructions:</h3>
+                        <p">${mealRecipe.strInstructions}</p>
+                    </div>
+                    <div class = "recipe-meal-img">
+                        <img src = "${mealRecipe.strMealThumb}" alt = "">
+                    </div>
+                    <div class = "recipe-link">
+                        <a href = "${mealRecipe.strYoutube}" target = "_blank">Watch Video</a>
+                    </div>
+                `;
+                mealDetailsContent.parentElement.classList.add('showRecipe');
+            })
+            .catch(error => console.error("Meal Recipe Fetch Error: ", error));
+    }
+};
+
 searchForm.addEventListener('submit', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
